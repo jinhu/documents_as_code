6bca41a443e6cb97cb3339f35737db4d607b2b05
jinmin hu <jinmin.hu@altran.com>
2020-03-05 11:56:00 +0100

    move back to src

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var app = express();
 
-app.use(express.static('.'));
+app.use(express.static('src'));
 
 var server = app.listen(8081, function(){
     var port = server.address().port;
